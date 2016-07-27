5778fd918e04c5a0e70b7f421d636d9a31fcd5e3
Alejandro Caballero <lava.caballero@gmail.com>
2016-07-27 00:03:44 -0500

    TinyMCE adjustments

--- media/global_functions.js
+++ media/global_functions.js
@@ -16,7 +16,7 @@ $_TINYMCE_ADDON_FUNCTIONS['emojione_for_bardcanvas'] = function($trigger, $form)
     var $strings = $('#emojione_for_bardcanvas_tinymce_strings');
     var _title    = $strings.find('.title').text();
     
-    var editor_id = $form.find('textarea.tinymce').attr('id');
+    var editor_id = $form.find('textarea[class*="tinymce"]').attr('id');
     var editor    = tinymce.get(editor_id);
     
     var url        = $_FULL_ROOT_PATH + '/emojione_for_bardcanvas/wp-emoji-one/popup.php';
