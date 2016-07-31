35a2afc016181fd5ee4188ee188ce38cfb2045dc
Alejandro Caballero <lava.caballero@gmail.com>
2016-07-31 03:37:20 -0500

    Reworked tinymce to allow multiple addon bars

--- media/global_functions.js
+++ media/global_functions.js
@@ -11,12 +11,12 @@ tinymce_default_css_files[tinymce_default_css_files.length]
     = $_FULL_ROOT_PATH + '/emojione_for_bardcanvas/media/global_styles~v' + $_SCRIPTS_VERSION + '.css';
 
 // Callback for the button below the editor
-$_TINYMCE_ADDON_FUNCTIONS['emojione_for_bardcanvas'] = function($trigger, $form)
+$_TINYMCE_ADDON_FUNCTIONS['emojione_for_bardcanvas'] = function($trigger, $container)
 {
     var $strings = $('#emojione_for_bardcanvas_tinymce_strings');
     var _title    = $strings.find('.title').text();
     
-    var editor_id = $form.find('textarea[class*="tinymce"]').attr('id');
+    var editor_id = $container.find('textarea[class*="tinymce"]').attr('id');
     var editor    = tinymce.get(editor_id);
     
     var url        = $_FULL_ROOT_PATH + '/emojione_for_bardcanvas/wp-emoji-one/popup.php';
